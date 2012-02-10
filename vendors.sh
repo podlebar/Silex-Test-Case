4defa84f7dabb26477ea6e51a21decfdc362f750
Bartosz Podlewski <b.podlewski@gmail.com>
2012-02-10 10:30:38 +0100

    fuck submodules

---
+++ vendors.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git clone https://github.com/fabpot/Silex.git vendor/silex
+git clone https://github.com/swiftmailer/swiftmailer.git vendor/swiftmailer
+git clone https://github.com/symfony/Form vendor/Symfony/Component/Form
+git clone https://github.com/symfony/TwigBridge vendor/Symfony/Bridge/Twig
+git clone https://github.com/symfony/Validator vendor/Symfony/Component/Validator
+git clone https://github.com/symfony/Translation.git vendor/Symfony/Component/Translation
+git clone https://github.com/symfony/Yaml.git vendor/Symfony/Component/Yaml
